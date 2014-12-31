e7743c7f08abbf07cae9e597f62c30b6f56e2185
jdgrant <jdgrant@Jon-Grants-MacBook-Pro-2.local>
2014-12-31 15:01:18 -0500

    calendar

---
+++ www/js/pages/compCalendar.js
@@ -0,0 +1,64 @@
+/*
+ *  Document   : compCalendar.js
+ *  Author     : pixelcave
+ *  Description: Custom javascript code used in Calendar page
+ */
+
+var CompCalendar = function() {
+    var calendarEvents  = $('.calendar-events');
+
+    /* Function for initializing drag and drop event functionality */
+    var initEvents = function() {
+        calendarEvents.find('li').each(function() {
+            // create an Event Object (http://arshaw.com/fullcalendar/docs/event_data/Event_Object/)
+            var eventObject = { title: $.trim($(this).text()), color: $(this).css('background-color') };
+
+            // store the Event Object in the DOM element so we can get to it later
+            $(this).data('eventObject', eventObject);
+
+            // make the event draggable using jQuery UI
+            $(this).draggable({ zIndex: 999, revert: true, revertDuration: 0 });
+        });
+    };
+
+    return {
+        init: function() {
+            /* Initialize drag and drop event functionality */
+            initEvents();
+
+            /* Add new event in the events list */
+            var eventInput      = $('#add-event');
+            var eventInputVal   = '';
+
+           
+            /* Initialize FullCalendar */
+            var date = new Date();
+            var d = date.getDate();
+            var m = date.getMonth();
+            var y = date.getFullYear();
+
+            
+			
+		
+						$('#calendar').fullCalendar({
+							header: {
+								left: 'title',
+								center: '',
+								right: 'today month,agendaWeek,agendaDay prev,next'
+							},
+							firstDay: 1,
+							defaultView: 'basicDay',
+							editable: false,
+							droppable: false,
+							events: "ajax/calendar.json"
+						});
+						
+			
+			
+			
+			
+			
+			
+        }
+    };
+}();
